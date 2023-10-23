f3378dc9c9dd86b9c18c33e0c835e1ba39beeeb4
JupriGH <pabuaran@gmail.com>
2023-10-23 10:19:41 +0700

    updates

--- lib/event.rb
+++ lib/event.rb
@@ -100,7 +100,10 @@ module Apify
 				
 			#event_name = maybe_extract_enum_member_value(event_name)
 
-			@_listeners_to_wrappers[event_name] ||= Set.new << listener
+			@_listeners_to_wrappers[event_name] ||= Set.new 
+			@_listeners_to_wrappers[event_name] << listener
+			p "#{event_name} => #{@_listeners_to_wrappers[event_name]}"
+			
 			#return self._event_emitter.add_listener(event_name, outer_wrapper)
 		end
 
